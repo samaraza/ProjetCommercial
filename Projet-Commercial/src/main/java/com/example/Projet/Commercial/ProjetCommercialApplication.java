48d44f10a06fb1459fe98e85560c882e13676642
samaraza <hilalisamaraza@gmail.com>
2024-04-17 23:35:09 +0200

    first commit

---
+++ Projet-Commercial/src/main/java/com/example/Projet/Commercial/ProjetCommercialApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projet.Commercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCommercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCommercialApplication.class, args);
+	}
+
+}
---
+++ Projet-Commercial/src/test/java/com/example/Projet/Commercial/ProjetCommercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projet.Commercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCommercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
